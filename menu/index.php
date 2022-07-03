579d009c3db909f217ee77db3578b70a50776fbf
diegoteos <diegoteos@hotmail.com>
2022-07-02 21:07:49 -0600

    menu adaptable

---
+++ menu/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'encabezado.php';
+
+?>
+<div class="container">
+    <h2> Esta es la pagina de inicio la cual es la principal, bienvenido.</h2>
+</div>
+
+<?php
+if (!empty($_POST['menu'])) {
+$pagina = $_POST['menu'];
+    switch ($pagina) {
+        case '1':
+            require_once 'inicio.php';
+            break;
+
+        case '2':
+            require_once 'consultas.php';
+            break;
+
+        case '3':
+            include_once 'https://sistemas.pnc.gob.gt/escritorio/index.php?r=vehiculos/';
+            break;
+
+        default:
+            echo "Escoja el tipo de consulta que quiere realizar";
+            break;
+    }
+}
+
+?>
