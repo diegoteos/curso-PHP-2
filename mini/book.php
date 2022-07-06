7d705206bf6371c6561be5f4c69f77252e1043d0
diegoteos <diegoteos@hotmail.com>
2022-07-05 22:04:51 -0600

    POO

--- mini/book.php
+++ mini/book.php
@@ -11,18 +11,30 @@ class Book
     // Metodos: ¿Que es lo que puede hacer nuestro objeto?
     // metodo constructor
     public function __construct(
-        public string $titulo,
-        public string $autor,
-        public float $precio,
-        public int $stok,
-        public int $id
-    )
+        private string $titulo,
+        private string $autor,
+        private float $precio,
+        private int $stok,
+        private int $id
+    ) 
+    {}
 
     public function getInfo()
     {
         // Escribir lo que este metodo hara
-        $info = "<b> $this->title</b><br/>";
-
+        $info = "<b> {$this->titulo} </b> escrito por: <i> $this->autor </i> <br/>
+        precio: {$this->precio} <br/>
+        ID: {$this->id} <br/>
+        ";
+        if ($this->stok >= 10) {
+            $info .= "Existencias:  <spam style='color: green'>  {$this->stok} </spam><br/> ";
+        } elseif ($this->stok >= 1 && $this->stok < 10) {
+            $info .= "Pocas existencias:  <spam style='color: orange'>  {$this->stok} </spam><br/> ";
+        }
+        else {
+            $info .= "Existencias:  <spam style='color: red'> {$this->stok} </spam><br/> ";
+        }
+        
         return $info;
     }
 }
@@ -33,21 +45,26 @@ $book1 = new Book(
     'Mi vida es linda',
     'Diego Teos.',
     299.90,
-    150,
+    $a = random_int(0, 30),
     1
 );
 
+// Le cambiamos precio si las propiedades son publicas
+//$book1->precio = 0.00;
+
+echo $book1->getInfo();
 
 $book2 = new Book(
     'Mi vida es linda',
     'Diego Teos.',
-    299.90,
-    150,
-    1
+    249.90,
+    $a = random_int(0, 30),
+    2
 );
 
 
-echo $book1->titulo;
+echo '<br/><br/>';
+echo $book2->getInfo();
 
 
 
