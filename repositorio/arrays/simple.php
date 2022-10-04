f45d38b82b8c49aab927459263e59181aaa956ed
diegoteos <diegoteos@hotmail.com>
2022-10-03 19:30:56 -0600

    organizacion de carpetas

--- repositorio/arrays/simple.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$esto = ['diego', 'teos', 'Analista de sistemas'];
-
-// esto es igual a: array(3) { [0]=> string(5) "diego" [1]=> string(4) "teos" [2]=> string(20) "Analista de sistemas" }
-
-var_dump($esto);
