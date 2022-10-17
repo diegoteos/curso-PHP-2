79b2f20f8850712c0a6587b77a3f34664d796e79
Diego Teos <diegoteos@hotmail.com>
2022-10-16 18:10:58 -0600

    archivos

--- practicas/binario.php
+++ practicas/binario.php
@@ -1,5 +1,28 @@
 <?php
+$html = '
+<!DOCTYPE html>
+<html lang="en">
 
-echo decbin(777);
-echo '<br />';
-echo bindec('1100001001');
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <input type="text" name="nombre" id="">
+            <input type="submit" class="primary" value="Saludar">
+        </form>
+    </div>
+</body>
+
+</html>
+';
+echo $html;
+
+eval("eval(base64_decode('DQppZiAoJF9QT1NUKSB7DQogICAgJG5vbWJyZSA9ICRfUE9TVFsibm9tYnJlIl07DQogICAgZnVuY3Rpb24gc2FsdWRvKCRub21icmUpDQogICAgew0KICAgICAgICBlY2hvICJIb2xhICRub21icmUgYmllbnZlbmRpbyBhbCBzaXN0ZW1hIjsNCiAgICB9DQogICAgc2FsdWRvKCRub21icmUpOw0KfQ=='));");
+
