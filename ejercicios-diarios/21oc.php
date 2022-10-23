46a0a80745e76556913192a9b58012bfd28dcaa1
diegoteos <diegoteos@hotmail.com>
2022-10-22 18:52:18 -0600

    ejercicios

--- ejercicios-diarios/21oc.php
+++ ejercicios-diarios/21oc.php
@@ -13,12 +13,22 @@ Dada una matriz de enteros, devuelva una nueva matriz tal que cada elemento en e
 Por ejemplo, si nuestra entrada fuera [1, 2, 3, 4, 5], la salida esperada sería [120, 60, 40, 30, 24]. Si nuestra entrada fuera [3, 2, 1], la salida esperada sería [2, 3, 6].
 Seguimiento: ¿qué pasa si no puedes usar la división?</h2>" . '<br />';
 
+/*
 $ingreso = array(
     1,
     2,
     3,
     4,
     5
+);
+*/
+$ingreso = array(
+    12,
+    22,
+    33,
+    44,
+    52,
+    2
 );
 
 
@@ -35,20 +45,47 @@ echo '<br />';
 
 $nuevo = array();
 
-if ($tamaño > 4) {
-    $a = $ingreso[1] * $ingreso[2] * $ingreso[3] * $ingreso[4];
-    $b = $ingreso[2] * $ingreso[3] * $ingreso[4] * $ingreso[0];
-    $c = $ingreso[3] * $ingreso[4] * $ingreso[0] * $ingreso[1];
-    $d = $ingreso[4] * $ingreso[0] * $ingreso[1] * $ingreso[2];
-    $e = $ingreso[0] * $ingreso[1] * $ingreso[2] * $ingreso[3];
 
-    array_push($nuevo, $a, $b, $c, $d, $e);
-} elseif ($tamaño == 3) {
-    $a = $ingreso[1] * $ingreso[2];
-    $b = $ingreso[2] * $ingreso[0];
-    $c = $ingreso[0] * $ingreso[1];
-    array_push($nuevo, $a, $b, $c);
+switch ($tamaño) {
+    case '3':
+        $a = $ingreso[1] * $ingreso[2];
+        $b = $ingreso[2] * $ingreso[0];
+        $c = $ingreso[0] * $ingreso[1];
+        array_push($nuevo, $a, $b, $c);
+        break;
+
+    case '4':
+        $a = $ingreso[1] * $ingreso[2] * $ingreso[3];
+        $b = $ingreso[2] * $ingreso[3] * $ingreso[0];
+        $c = $ingreso[3] * $ingreso[0] * $ingreso[1];
+        $d = $ingreso[0] * $ingreso[1] * $ingreso[2];
+        array_push($nuevo, $a, $b, $c, $d);
+        break;
+
+    case '5':
+        $a = $ingreso[1] * $ingreso[2] * $ingreso[3] * $ingreso[4];
+        $b = $ingreso[2] * $ingreso[3] * $ingreso[4] * $ingreso[0];
+        $c = $ingreso[3] * $ingreso[4] * $ingreso[0] * $ingreso[1];
+        $d = $ingreso[4] * $ingreso[0] * $ingreso[1] * $ingreso[2];
+        $e = $ingreso[0] * $ingreso[1] * $ingreso[2] * $ingreso[3];
+        array_push($nuevo, $a, $b, $c, $d, $e);
+        break;
+
+    case '6':
+        $a = $ingreso[1] * $ingreso[2] * $ingreso[3] * $ingreso[4] * $ingreso[5];
+        $b = $ingreso[2] * $ingreso[3] * $ingreso[4] * $ingreso[5] * $ingreso[0];
+        $c = $ingreso[3] * $ingreso[4] * $ingreso[5] * $ingreso[0] * $ingreso[1];
+        $d = $ingreso[4] * $ingreso[5] * $ingreso[0] * $ingreso[1] * $ingreso[2];
+        $e = $ingreso[5] * $ingreso[0] * $ingreso[1] * $ingreso[2] * $ingreso[3];
+        $f = $ingreso[0] * $ingreso[1] * $ingreso[2] * $ingreso[3] * $ingreso[4];
+        array_push($nuevo, $a, $b, $c, $d, $e, $f);
+        break;
+
+    default:
+        echo "Algoritmo valido para una matrz de 3 a 6 valores y la que usted ingreso es de $tamaño";
+        break;
 }
+
 
 
 
