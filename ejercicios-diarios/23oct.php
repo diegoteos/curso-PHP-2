abde5a76cfcd2e2df6d32e463832acd369e09446
diegoteos <diegoteos@hotmail.com>
2022-10-23 19:57:59 -0600

    23 de octubre

---
+++ ejercicios-diarios/23oct.php
@@ -0,0 +1,24 @@
+
+<?php
+
+echo "<h2>
+Good morning! Here's your coding interview problem for today.
+This problem was asked by Stripe.
+Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+You can modify the input array in-place.
+
+</h2>" . '<br />';
+echo '<br /><br />';
+echo "<h2>
+
+
+¡Buenos dias! Aquí está su problema de entrevista de codificación para hoy.
+Este problema fue preguntado por Stripe.
+Dada una matriz de enteros, encuentre el primer entero positivo faltante en tiempo lineal y espacio constante. En otras palabras, encuentre el entero positivo más bajo que no existe en la matriz. La matriz también puede contener duplicados y números negativos.
+Por ejemplo, la entrada [3, 4, -1, 1] debería dar 2. La entrada [1, 2, 0] debería dar 3.
+Puede modificar la matriz de entrada en el lugar.
+
+</h2>" . '<br />';
+
+
