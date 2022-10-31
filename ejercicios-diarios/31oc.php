1f2a070a6e9c0ec6d08525bf81ed0ac1b25ac795
diegoteos <diegoteos@hotmail.com>
2022-10-31 16:58:07 -0600

    31 octubere

---
+++ ejercicios-diarios/31oc.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "<h2>English: Good morning! Here's your coding interview problem for today.
+This problem was asked by Amazon.
+There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+For example, if N is 4, then there are 5 unique ways:
+•	1, 1, 1, 1
+•	2, 1, 1
+•	1, 2, 1
+•	1, 1, 2
+•	2, 2
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+" . '<br />';
+echo '<br /><br />';
+echo "<h2>Español: ¡Buenos dias! Aquí está su problema de entrevista de codificación para hoy.
+Este problema fue pedido por Amazon.
+Existe una escalera con N escalones, y puedes subir 1 o 2 escalones a la vez. Dado N, escribe una función que devuelva el número de formas únicas en que puedes subir la escalera. El orden de los pasos importa.
+Por ejemplo, si N es 4, entonces hay 5 formas únicas:
+• 1, 1, 1, 1
+• 2, 1, 1
+• 1, 2, 1
+• 1, 1, 2
+• 2, 2
+¿Qué pasaría si, en lugar de poder subir 1 o 2 escalones a la vez, pudieras subir cualquier número de un conjunto de enteros positivos X? Por ejemplo, si X = {1, 3, 5}, podrías subir 1, 3 o 5 escalones a la vez.
+</h2>" . '<br />';
