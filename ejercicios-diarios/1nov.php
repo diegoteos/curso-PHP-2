e6453712b5604210dd7973187d15fb85f63872da
diegoteos <diegoteos@hotmail.com>
2022-11-01 17:24:44 -0600

    ejercicio 1 nov

---
+++ ejercicios-diarios/1nov.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h2>English: Good morning! Here's your coding interview problem for today.
+This problem was asked by Amazon.
+Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+For example, given s = 'abcba' and k = 2, the longest substring with k distinct characters is 'bcb'.
+
+" . '<br />';
+echo '<br /><br />';
+echo "<h2>Español: ¡Buenos dias! Aquí está su problema de entrevista de codificación para hoy.
+Este problema fue pedido por Amazon.
+Existe una escalera con N escalones, y puedes subir 1 o 2 escalones a la vez. Dado N, escribe una función que devuelva el número de formas únicas en que puedes subir la escalera. El orden de los pasos importa.
+Por ejemplo, si N es 4, entonces hay 5 formas únicas:
+• 1, 1, 1, 1
+• 2, 1, 1
+• 1, 2, 1
+• 1, 1, 2
+• 2, 2
+¿Qué pasaría si, en lugar de poder subir 1 o 2 escalones a la vez, pudieras subir cualquier número de un conjunto de enteros positivos X? Por ejemplo, si X = {1, 3, 5}, podrías subir 1, 3 o 5 escalones a la vez.
+</h2>" . '<br />';
