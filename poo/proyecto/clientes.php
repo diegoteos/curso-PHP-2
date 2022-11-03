fa34f0f0885dae82e543055abb29fb863aa5d31d
diegoteos <diegoteos@hotmail.com>
2022-11-02 23:19:40 -0600

    POO

--- poo/proyecto/clientes.php
+++ poo/proyecto/clientes.php
@@ -20,11 +20,39 @@ class Clientes
         $this->email = $email_externo;
     }
 
-    public function verCliente(){
+    public function verCliente()
+    {
         $datos = array(
             $this->dpi,
             $this->email
         );
         return  $datos;
+    }
+    /* FUNCIONES PARA VER LOS ATRIBUTOS 
+    public function verDPI()
+    {
+        return  $this->dpi;
+    }
+    public function verEmail()
+    {
+        return  $this->email;
+    }
+    */
+    // MODIFICAR ATRIBUTOS
+    public function getDPI()
+    {
+        return  $this->dpi;
+    }
+    public function setDPI($dpi_externo)
+    {
+        $this->dpi = $dpi_externo;
+    }
+    public function getEmail()
+    {
+        return  $this->email;
+    }
+    public function setEmail($email_externo)
+    {
+        $this->email = $email_externo;
     }
 }
--- poo/proyecto/dos.php
+++ poo/proyecto/dos.php
@@ -6,11 +6,33 @@ include 'clientes.php';
 
 $cliente = new Clientes('2323232323', 'josls@info.com');
 
+
+/*
 echo '<pre>';
-var_dump($cliente->verCliente());
-echo '<br />';
-var_dump($cliente->verCliente());
 echo '<br />';
 print_r($cliente->verCliente());
+echo '</pre>';
+echo '<pre>';
 echo '<br />';
-print_r($cliente->verCliente());
+print_r('DPI: ' . $cliente->verDPI());
+echo '</pre>';
+echo '<pre>';
+echo '<br />';
+print_r('Email: ' . $cliente->verEmail());
+echo '</pre>';
+*/
+// generarlo formato json 
+//echo json_encode($cliente->verCliente());
+
+
+print_r('DPI: ' . $cliente->getDPI());
+echo '<br />';
+print_r('Email: ' . $cliente->getEmail());
+
+$cliente->setDPI('9999999A');
+$cliente->setEmail('eso@info.cm');
+echo '<br />';
+echo '<br />';
+print_r('DPI corregiedo: ' . $cliente->getDPI());
+echo '<br />';
+print_r('Email corregiedo: ' . $cliente->getEmail());
