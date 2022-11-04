d4428566ebf936187b0262484c979ca2efc5de0c
diegoteos <diegoteos@hotmail.com>
2022-11-03 22:17:15 -0600

    años bisiestos

--- ejercicios/años_viciestos.php
+++ ejercicios/años_viciestos.php
@@ -1,1 +1,23 @@
 <?php
+
+
+function añoBisiesto($año)
+{
+
+    $contador = "0";
+
+    for ($i = $año; $i < 2200; $i++) {
+        if ($i % 4) {
+            //echo "Año: $i <br />";
+        } else {
+            echo $contador + 1 . " El año: $i es biciesto <br />";
+            $contador++;
+            if ($contador == 30) {
+                echo "este fue el ultimo";
+                die();
+            }
+        }
+    }
+}
+
+añoBisiesto(2021);
