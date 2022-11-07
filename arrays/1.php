b43b12095313e44f56d287aad5ce2dc3a17fd082
diegoteos <diegoteos@hotmail.com>
2022-11-06 19:29:47 -0600

    arrays

--- arrays/1.php
+++ arrays/1.php
@@ -5,6 +5,14 @@ $persona = array(
     "nombre2" => "carlos",
     "apellido" => "jjan"
 );
+$persona2 = array(
+    "armando",
+    "carlos",
+    "jjan"
+);
 echo implode(' ', $persona) . "\n";
 echo '<pre>';
 var_dump($persona);
+echo '<br>';
+var_dump($persona2);
+
