fac4f3013e413ef591e758ef0500995ae3ec952f
diegoteos <diegoteos@hotmail.com>
2022-11-08 15:19:31 -0600

    array asociativo e indexado

---
+++ arrays/array_asociativo.php
@@ -0,0 +1,10 @@
+<?php
+// array Asociativo
+$estudiante = array(
+    'primerNombre' => 'Diego',
+    'segundoNombre' => 'Armando',
+    'primerApellido' => 'Maradona',
+    'segundoApellido' => 'Lopez',
+ );
+
+ echo $estudiante['primerNombre'] . ' ' . $estudiante['segundoNombre'] . ' ' . $estudiante['primerApellido'] . ' ' . $estudiante['segundoApellido'];
---
+++ arrays/array_indexado.php
@@ -0,0 +1,20 @@
+<?php
+// array indexado
+
+$nombre = 'Juan armando';
+$apellido = 'Maradona lopez';
+
+$trabajador = array(
+$nombre,
+$apellido
+);
+
+$estudiante = array(
+    'Diego', 'Armando', 'Maradona', 'Lopez'
+ );
+
+
+
+ echo $estudiante[0] . ' ' . $estudiante[1] . ' ' . $estudiante[2] . ' ' . $estudiante[3] . '<br />';
+
+ echo $trabajador[0] . ' ' . $trabajador[1];
