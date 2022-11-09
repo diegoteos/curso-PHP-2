a0c2c403d0efc50029e7ac787c8bf8609f911b9c
diegoteos <diegoteos@hotmail.com>
2022-11-08 22:41:07 -0600

    api

---
+++ arrays/str_split.php
@@ -0,0 +1,7 @@
+<?php
+
+// str_split — Convierte un string en un array
+
+
+$saludo = "hola, mucho gusto de saludar, hasta pronto.";
+var_dump(str_split($saludo, 8)); // definimos cuantos caracteres tendra cada array
