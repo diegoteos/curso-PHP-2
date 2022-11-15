3b05c514b8f82f9df8335e5cea4d856e524351a9
diegoteos <diegoteos@hotmail.com>
2022-11-15 11:25:17 -0600

    corchetes

---
+++ ejercicios-diarios/27.php
@@ -0,0 +1,32 @@
+<?php
+
+$uno = '{';
+$dos = '}';
+$tres = '[';
+$cuatro = ']';
+$cinco = '(';
+$seis = ')';
+
+$cadena = '([])[]({})';
+$tamaño = strlen($cadena);
+$contador = 0;
+if (strlen($cadena) % 2 == 0) {
+
+    for ($i = 0; $i < strlen($cadena); $i++) {
+        $contador++;
+        if ($cadena[$i] == $uno && $cadena[$i + 1] != $dos) {
+            echo "una no es correcta";
+        }
+        if ($cadena[$i] == $tres && $cadena[$i + 1] != $cuatro) {
+            echo "dos no es correcta";
+        }
+        if ($cadena[$i] == $cinco && $cadena[$i + 1] != $seis) {
+            echo "tres no es correcta";
+        }
+        if ($contador == $tamaño) {
+            echo "verdadero";
+        }
+    }
+} else {
+    echo "falso";
+}
