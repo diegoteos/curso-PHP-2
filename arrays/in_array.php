e872f4a904649642a8e12dfee018fcb7909a7476
diegoteos <diegoteos@hotmail.com>
2022-11-16 21:12:26 -0600

    Update in_array.php

--- arrays/in_array.php
+++ arrays/in_array.php
@@ -6,3 +6,21 @@ $array = array('hola', 'mundo', 'cruel', 2022);
 if (in_array('hola', $array)) {
     echo "hola si que si existe";
 }
+
+$uno = 'hola mundo';
+$dos = array();
+
+//echo strlen($uno);
+
+for ($i=0; $i < strlen($uno); $i++) { 
+
+    array_push($dos, $uno[$i]);
+}
+echo '<pre>';
+print_r($dos);
+
+if (in_array('u', $dos)) {
+echo "si existe";    
+}
+
+
