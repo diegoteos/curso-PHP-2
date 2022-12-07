8563b5c6253e27d5ed439966d5ac8a4d9b600355
diegoteos <diegoteos@hotmail.com>
2022-12-06 20:40:21 -0600

    Update 49.php

--- ejercicios-diarios/49.php
+++ ejercicios-diarios/49.php
@@ -1,7 +1,7 @@
 <h2>Dada una matriz de números, encuentre la suma máxima de cualquier subarreglo contiguo de la matriz.
-Por ejemplo, dada la matriz [34, -50, 42, 14, -5, 86], la suma máxima sería 137, ya que tomaríamos los elementos 42, 14, -5 y 86.
-Dada la matriz [-5, -1, -8, -9], la suma máxima sería 0, ya que no tomaríamos ningún elemento.
-Haz esto en tiempo O(N).</h2>
+    Por ejemplo, dada la matriz [34, -50, 42, 14, -5, 86], la suma máxima sería 137, ya que tomaríamos los elementos 42, 14, -5 y 86.
+    Dada la matriz [-5, -1, -8, -9], la suma máxima sería 0, ya que no tomaríamos ningún elemento.
+    Haz esto en tiempo O(N).</h2>
 
 <?php
 
@@ -13,4 +13,9 @@ $suma = array_sum($matriz);
 
 // creamos subarreglos 
 
-echo $suma;
+// recorremos el arreglo para crear un subarreglo
+
+
+for ($i = 0; $i <= count($matriz) - 1; $i++) {
+    
+}
